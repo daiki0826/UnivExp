3549ce33a63c018d21d4da17d5fa902c593e249b
daiki0826 <daiki.baske08@icloud.com>
2021-12-22 17:49:52 +0900

    SolveLP_WA　作った

---
+++ src/SolveLP_WA.java
@@ -0,0 +1,25 @@
+import ilog.concert.IloException;
+import ilog.cplex.IloCplex;
+
+public class SolveLP_WA {
+    
+    public SolveLP_WA(String file){
+        try {
+            IloCplex cplex=new IloCplex();
+		    cplex.setParam(IloCplex.IntParam.MIPDisplay, 0);
+
+            //解くlpファイル指定
+			cplex.importModel(file); 
+			//lpファイルを解く
+			cplex.solve();	
+
+            //目的関数の値を取得
+			double objval = cplex.getObjValue();
+			System.out.println("目的関数 :"+objval);
+            
+        } catch (IloException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+    }
+}
